999212e3c9fa314c2e930d6ff1cb954cdf55563e
Tahsin Haider <tahsin3194@gmail.com>
2025-11-12 14:51:18 +0500

    Test: simulate pass test

--- __test__/userApi.test.js
+++ __test__/userApi.test.js
@@ -3,7 +3,7 @@ import request from "supertest";
 import app from "../server/config_server.js";
 
 
-describe("1User API Tests", () => {
+describe("User API Tests", () => {
   beforeEach(() => {
     inMemStorage.length = 0;
     inMemStorage.push(
@@ -15,7 +15,7 @@ describe("1User API Tests", () => {
    it("GET / should return backend alive message", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("BACKEND ISUNNING SUCCESSFULLY!");
+    expect(res.text).toBe("BACKEND IS RUNNING SUCCESSFULLY!");
   });
 
   it("should return user data if user exists", async () => {
