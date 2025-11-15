643fb0ff47833008fb2ea58ce11521656d646785
Tahsin Haider <tahsin3194@gmail.com>
2025-11-15 20:26:04 +0500

    Update: test change from legacy inmemory

--- backend/__test__/userApi.test.js
+++ backend/__test__/userApi.test.js
@@ -1,32 +1,10 @@
-import { inMemStorage } from "../server/config_server.js";
 import app from "../server/config_server.js";
 import request from "supertest";
 
-
 describe("User API Tests", () => {
-  beforeEach(() => {
-    inMemStorage.length = 0;
-    inMemStorage.push(
-      { id: 1, name: "Tahsin", age: 24, profession: "Developer" },
-      { id: 2, name: "Ali", age: 30, profession: "Designer" }
-    );
-  });
-
-   it("GET / should return backend alive message", async () => {
+  it("GET / should return backend alive message", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe("BACKEND IS RUNNING SUCCESSFULLY!");
-  });
-
-  it("should return user data if user exists", async () => {
-    const res = await request(app).get("/api/getUser/1");
-
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual({
-      id: 1,
-      name: "Tahsin",
-      age: 24,
-      profession: "Developer",
-    });
   });
 });
